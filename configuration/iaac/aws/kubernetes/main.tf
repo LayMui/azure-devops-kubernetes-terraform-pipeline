1ce2aee6db13fd46114b4faa1b9cea022eb02ebb
LayMui <laymui.toh@taiger.com>
2020-07-23 20:50:01 +0800

    Fix the terraform error

--- configuration/iaac/aws/kubernetes/main.tf
+++ configuration/iaac/aws/kubernetes/main.tf
@@ -27,6 +27,7 @@ provider "kubernetes" {
   token                  = data.aws_eks_cluster_auth.cluster.token
   load_config_file       = false
   version                = "~> 1.9"
+  features {}
 }
 
 module "in28minutes-cluster" {
